22d7e9992b7dfa0c24a8640ae9908db00139e069
Mufaddal Dairkee <mdairkee@edu.uwaterloo.ca>
2018-04-10 16:22:24 -0400

    weekly database

---
+++ SelfControl/SelfControl/DatabaseManager/WeeklyReviewDatabse.cs
@@ -0,0 +1,31 @@
+﻿using SelfControl.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfControl.DatabaseManager
+{
+    public class WeeklyReviewDatabse : ConnectionManager
+    {
+        public WeeklyReviewDatabse(string filePath) : base(filePath)
+        {
+            database.CreateTableAsync<WeeklyReviewTable>().Wait();
+        }
+
+        public Task<int> SaveItemAsync(WeeklyReviewTable item)
+        {
+            return database.InsertAsync(item);
+        }
+
+        public Task<int> UpdateItemAsync(WeeklyReviewTable item)
+        {
+            return database.UpdateAsync(item);
+        }
+
+        public Task<int> DeleteItemAsync(WeeklyReviewTable item)
+        {
+            return database.DeleteAsync(item);
+        }
+    }
+}
---
+++ SelfControl/SelfControl/Models/WeeklyReviewTable.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfControl.Models
+{
+    [Table("WeeklyReview")]
+    public class WeeklyReviewTable
+    {
+        static string DATETIME_COL = "_date";
+        static string ID_COL = "_id";
+        static string PIC_ID = "_picId";
+        static string RESPONSE_COL = "_response";
+
+        public static string dateTimeCol { get => DATETIME_COL; }
+        public static string idCol { get => ID_COL; }
+        public static string picIdCol { get => PIC_ID; }
+        public static string responseCol { get => RESPONSE_COL; }
+
+        [Column("_id")]
+        public int ID { get; set; }
+        [Column("_picId")]
+        public int PICID { get; set; }
+        [Column("_date")]
+        public DateTime DATE { get; set; }
+        [Column("_response")]
+        public string RESPONSE { get; set; }
+    }
+}
