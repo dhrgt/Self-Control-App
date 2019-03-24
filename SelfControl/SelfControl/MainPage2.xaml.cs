166acea0941c99a27ad513c7d0eea1cc6cb7d075
d27gupta <d27gupta@edu.uwaterloo.ca>
2019-03-24 16:35:25 -0400

    CREATE TABBED LAYOUT

---
+++ SelfControl/SelfControl/MainPage2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SelfControl
+{
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
